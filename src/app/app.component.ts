fe948d1c3378ae0dd6b97fcf086bc732fe200082
anju-chauhan <anju.chauhan@roadcast.net>
2022-06-27 13:05:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,124 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'type';
+  items=[
+  {
+  name:'Women',
+  img:'https://images.pexels.com/photos/12169955/pexels-photo-12169955.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+   },
+{
+    name:'Men',
+    img:'https://images.pexels.com/photos/12169955/pexels-photo-12169955.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+
+},
+{
+  name:'Accessories',
+    img:'https://images.pexels.com/photos/12169955/pexels-photo-12169955.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+
 }
+  
+];
+pics=[
+{
+  take:'Esprit Ruffle Shirt',
+  doller:'$16.64',
+  mix:'https://images.pexels.com/photos/1139627/pexels-photo-1139627.jpeg?auto=compress&cs=tinysrgb&w=600',
+},
+{
+  take:'Herschel supply',
+  doller:'$35.31',
+  mix:'https://images.pexels.com/photos/1139627/pexels-photo-1139627.jpeg?auto=compress&cs=tinysrgb&w=600',
+},
+{
+  take:'Only Check Trouser',
+  doller:'$16.64',
+  mix:'https://images.pexels.com/photos/1139627/pexels-photo-1139627.jpeg?auto=compress&cs=tinysrgb&w=600',
+},
+{
+  take:'Classic Trench Coat',
+  doller:'$16.64',
+  mix:'https://images.pexels.com/photos/1139627/pexels-photo-1139627.jpeg?auto=compress&cs=tinysrgb&w=600',
+
+}
+
+];
+level=[
+{
+  hit:'Front Pocket Jumper',
+  val:'$22.66',
+  off:'https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  hit:'Front Pocket Jumper',
+  val:'$22.66',
+  off:'https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  hit:'Front Pocket Jumper',
+  val:'$22.66',
+  off:'https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  hit:'Front Pocket Jumper',
+  val:'$22.66',
+  off:'https://images.pexels.com/photos/1557843/pexels-photo-1557843.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}
+];
+
+next=[
+{
+  kick:'Converse All Star Hi Plimsolls',
+  pam:'$34.98',
+  mob:'https://images.pexels.com/photos/884979/pexels-photo-884979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  kick:'Converse All Star Hi Plimsolls',
+  pam:'$34.98',
+  mob:'https://images.pexels.com/photos/884979/pexels-photo-884979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  kick:'Converse All Star Hi Plimsolls',
+  pam:'$34.98',
+  mob:'https://images.pexels.com/photos/884979/pexels-photo-884979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  kick:'Converse All Star Hi Plimsolls',
+  pam:'$34.98',
+  mob:'https://images.pexels.com/photos/884979/pexels-photo-884979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+}
+];
+up=[
+{
+  lek:'T-Shirt with Sleeve',
+  min:'$76.99',
+  deg:'https://images.pexels.com/photos/1100372/pexels-photo-1100372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+
+  lek:'T-Shirt with Sleeve',
+  min:'$76.99',
+  deg:'https://images.pexels.com/photos/1100372/pexels-photo-1100372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+
+  lek:'T-Shirt with Sleeve',
+  min:'$76.99',
+  deg:'https://images.pexels.com/photos/1100372/pexels-photo-1100372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},
+{
+  
+  lek:'T-Shirt with Sleeve',
+  min:'$76.99',
+  deg:'https://images.pexels.com/photos/1100372/pexels-photo-1100372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+}
+
+
+
+
+
+]
+
+
+
+}
+
